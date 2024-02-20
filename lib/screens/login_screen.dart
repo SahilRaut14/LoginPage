5c9fa73151a5ff70afd211ffdd9f5721cb5a08ed
SahilRaut14 <hilsa.raut@gmail.com>
2024-02-20 15:49:55 +0530

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:login/components/mybutton.dart';
+import 'package:login/components/square_tile.dart';
+import 'package:login/components/textfield.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final userNameController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  void signInUser() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // after finishing the ui; main axis to center was added so that whenthis app runs on different devices you hvae everything centered!
+
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
+                Icons.lock,
+                size: 100.0,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Welcome Back you've been missed!",
+                style: TextStyle(color: Colors.grey[700], fontSize: 16),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              MyTextField(
+                controller: userNameController,
+                hintText: "Username",
+                obscureText: false,
+
+                ///obscure means hidden
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot Password",
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              Mybutton(
+                onTap: signInUser,
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 1,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        "Or Continue with",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 1,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(
+                    imagePath: "images/apple.png",
+                  ),
+                  SizedBox(width: 60.0),
+                  SquareTile(
+                    imagePath: "images/google.png",
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Not a member?",
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    "Register now",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
